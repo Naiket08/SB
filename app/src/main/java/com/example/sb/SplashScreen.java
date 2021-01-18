9b3182410401767b3a2edf3da0b029d50d86e45c
Naiket08 <60822686+Naiket08@users.noreply.github.com>
2021-01-18 19:05:23 +0530

    Splash Screen and Terms and Conditions Screen Ready

--- app/src/main/java/com/example/sb/SplashScreen.java
+++ app/src/main/java/com/example/sb/SplashScreen.java
@@ -5,15 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageView;
 
 public class SplashScreen extends AppCompatActivity {
 
-    ImageView imageViewEllipse11,imageViewEllipse12,imageViewEllipse13,imageViewEllipse14,imageViewEllipse15,imageViewEllipse16;
+    ImageView imageViewEllipse11,imageViewEllipse12,imageViewEllipse13,imageViewEllipse14,imageViewEllipse15,imageViewEllipse16,imageViewLogo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash_screen);
 
         imageViewEllipse11 = (ImageView)findViewById(R.id.imageViewEllipse11);
@@ -22,6 +26,7 @@ public class SplashScreen extends AppCompatActivity {
         imageViewEllipse14 = (ImageView)findViewById(R.id.imageViewEllipse14);
         imageViewEllipse15 = (ImageView)findViewById(R.id.imageViewEllipse15);
         imageViewEllipse16 = (ImageView)findViewById(R.id.imageViewEllipse16);
+        imageViewLogo = (ImageView)findViewById(R.id.imageViewLogo);
 
         new Handler().postDelayed(new Runnable() {
             @Override
--- app/src/main/java/com/example/sb/TermsAndConditions.java
+++ app/src/main/java/com/example/sb/TermsAndConditions.java
@@ -3,12 +3,34 @@ package com.example.sb;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class TermsAndConditions extends AppCompatActivity {
+
+    ImageView imageViewWaveTopTandC;
+    TextView textViewTermsandConditions,textViewtandc1,textViewtandc2,textViewtandc3,textViewtandc4,textViewAcceptTandC;
+    CheckBox checkBoxAccept;
+    Button buttonSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_terms_and_conditions);
+
+        imageViewWaveTopTandC = (ImageView)findViewById(R.id.imageViewWaveTopTandC);
+        textViewTermsandConditions = (TextView)findViewById(R.id.textViewTermsandConditions);
+        textViewtandc1 = (TextView)findViewById(R.id.textViewtandc1);
+        textViewtandc2 = (TextView)findViewById(R.id.textViewtandc2);
+        textViewtandc3 = (TextView)findViewById(R.id.textViewtandc3);
+        textViewtandc4 = (TextView)findViewById(R.id.textViewtandc4);
+        textViewAcceptTandC = (TextView)findViewById(R.id.textVeiwAcceptTandC);
+        checkBoxAccept = (CheckBox)findViewById(R.id.checkBoxAccept);
+        buttonSubmit = (Button)findViewById(R.id.buttonSubmit);
     }
 }
