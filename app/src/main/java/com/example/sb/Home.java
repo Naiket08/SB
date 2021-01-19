5c5b6f4b3dad197ed579d09284ab321a6cf01ab3
Naiket08 <60822686+Naiket08@users.noreply.github.com>
2021-01-19 20:45:08 +0530

    Home Page added

---
+++ app/src/main/java/com/example/sb/Home.java
@@ -0,0 +1,51 @@
+package com.example.sb;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        //loading the default fragment
+        loadFragment(new FragmentGeneral());
+        //BottomNavigationMenu
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment = null;
+
+                switch (item.getItemId()){
+                    case R.id.navigation_general:
+                        fragment = new FragmentGeneral();
+                        break;
+                    case R.id.navigation_rooms:
+                        fragment = new FragmentRooms();
+                        break;
+                }
+                return loadFragment(fragment);
+            }
+        });
+    }
+
+    private boolean loadFragment(Fragment fragment){
+        if(fragment!=null){
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container,fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+}
