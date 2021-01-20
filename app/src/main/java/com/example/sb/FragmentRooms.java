6d2f8d0b715ee925a977a3addb27ebe1ef2aab95
NadeemAhemad <58600499+NadeemAhemad@users.noreply.github.com>
2021-01-20 15:06:43 +0530

    donw with todays work

--- app/src/main/java/com/example/sb/FragmentRooms.java
+++ app/src/main/java/com/example/sb/FragmentRooms.java
@@ -1,18 +1,55 @@
 package com.example.sb;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 public class FragmentRooms extends Fragment {
+
+ImageView imgeviewfragmentroom,noroom;
+TextView textViewfragmentroom;
+Button floatingbutton;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+       View v=inflater.inflate(R.layout.fragment_rooms,container,false);
+       //main content
+
+        imgeviewfragmentroom=(ImageView)v.findViewById(R.id.imgeviewfragmentroom);
+        noroom=(ImageView)v.findViewById(R.id.noroom);
+        textViewfragmentroom=(TextView)v.findViewById(R.id.textViewfragmentroom);
+        floatingbutton=(Button)v.findViewById(R.id.floatigbutton);
+
+        floatingbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Fragment newFragment = new FragmentRoomsSecond();
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+
+                transaction.replace(R.id.fragment_container,newFragment);
+                transaction.addToBackStack(null);
+
+                transaction.commit();
+
+
+
+
+            }
+        });
+
+
+       return v;
     }
 }
