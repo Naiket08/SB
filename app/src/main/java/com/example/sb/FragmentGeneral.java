05c152ee42945d842bb89061d8cf6c915a8cc470
NadeemAhemad <58600499+NadeemAhemad@users.noreply.github.com>
2021-01-24 17:23:26 +0530

    Changes Fucked Me

--- app/src/main/java/com/example/sb/FragmentGeneral.java
+++ app/src/main/java/com/example/sb/FragmentGeneral.java
@@ -1,6 +1,7 @@
 package com.example.sb;
 
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,6 +10,8 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
@@ -17,12 +20,15 @@ import org.w3c.dom.Text;
 public class FragmentGeneral extends Fragment {
 
     TextView textViewFavourite,textViewOtherTitle;
-    Button buttonAddRooms,buttonCategory,buttonAboutUs,buttonContactUs;
+    Button buttonAddRooms,buttonCategory,buttonAboutUs,buttonContactUs,buttonBackGeneral,buttonHamburgerGeneral,buttonKillGeneral;
+    DrawerLayout drawerLayout;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_general,container,false);
+
+        drawerLayout = (DrawerLayout)view.findViewById(R.id.drawer_layout);
 
         textViewFavourite = (TextView)view.findViewById(R.id.textViewFavourite);
         textViewOtherTitle = (TextView)view.findViewById(R.id.textViewOtherTitle);
@@ -30,6 +36,18 @@ public class FragmentGeneral extends Fragment {
         buttonCategory = (Button)view.findViewById(R.id.buttonCategory);
         buttonAboutUs = (Button)view.findViewById(R.id.buttonAboutUs);
         buttonContactUs = (Button)view.findViewById(R.id.buttonContactUs);
+
+        //AppBar Buttons
+        buttonBackGeneral = (Button)view.findViewById(R.id.buttonBackGeneral);
+        buttonHamburgerGeneral = (Button)view.findViewById(R.id.buttonHamburgerGeneral);
+        buttonKillGeneral = (Button)view.findViewById(R.id.buttonKillGeneral);
+
+        buttonBackGeneral.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().onBackPressed();
+            }
+        });
 
         buttonCategory.setOnClickListener(new View.OnClickListener() {
             @Override
