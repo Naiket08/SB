201ad2ae4b3c57a362687d13be506f7710170c77
NadeemAhemad <58600499+NadeemAhemad@users.noreply.github.com>
2021-01-30 13:35:00 +0530

    Update CustomAdapter.java

--- app/src/main/java/com/example/sb/CustomAdapter.java
+++ app/src/main/java/com/example/sb/CustomAdapter.java
@@ -6,13 +6,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -45,26 +50,33 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder
         holder.imageViewRecycleViewMyRoomEdit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(context,"Clicked",Toast.LENGTH_SHORT).show();
-                holder.name.setEnabled(true);
-                holder.name.setCursorVisible(true);
-                holder.name.requestFocus();
-                holder.name.setCursorVisible(true);
-                holder.name.setImeOptions(EditorInfo.IME_ACTION_DONE);
+                BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(context);
+                View parentView = LayoutInflater.from(context).inflate(R.layout.dailogue_predefine, null);
+                //View parentView = getLayoutInflater().inflate(R.layout.dailogue_predefine, null);
+                ImageView canceldailogpredefine = (ImageView) parentView.findViewById(R.id.canceldailogpredefine);
+                Button buttondailogprtedefine = (Button) parentView.findViewById(R.id.buttondailogpredefine);
+                EditText editTextdailogpredefine = (EditText) parentView.findViewById(R.id.editTextdailogpredefine);
+                bottomSheetDialog.setContentView(parentView);
+                bottomSheetDialog.show();
 
-            }
-        });
-        holder.imageViewRecycleViewMyRoomEdit.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View view, boolean b) {
+                canceldailogpredefine.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        bottomSheetDialog.cancel();
+                    }
+                });
 
-                if (!b) {
-                    holder.name.setEnabled(false);
-                    holder.name.requestFocus();
-                    holder.name.setCursorVisible(false);
-                    holder.name.setImeOptions(EditorInfo.IME_ACTION_DONE);
-                }
+                buttondailogprtedefine.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        holder.name.setEnabled(true);
+                        String s2 = editTextdailogpredefine.getText().toString();
+                        holder.name.setText(s2);
+                        holder.name.setEnabled(false);
+                        bottomSheetDialog.cancel();
 
+                    }
+                });
 
             }
         });
