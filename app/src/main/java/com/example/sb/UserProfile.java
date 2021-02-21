0a1b2f512db6e6b0e9b677a51739bf2493ea3cf0
Naiket08 <60822686+Naiket08@users.noreply.github.com>
2021-02-21 21:50:48 +0530

    Changes in User Profile page

--- app/src/main/java/com/example/sb/UserProfile.java
+++ app/src/main/java/com/example/sb/UserProfile.java
@@ -1,17 +1,27 @@
 package com.example.sb;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class UserProfile extends AppCompatActivity {
 
@@ -43,6 +53,28 @@ public class UserProfile extends AppCompatActivity {
         spinnerLoggedInDevices = (Spinner)findViewById(R.id.spinnerLoggedInDevices);
 
         mAuth = FirebaseAuth.getInstance();
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Users").child(mAuth.getCurrentUser().getUid()).child("User Details");
+        reference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String email = dataSnapshot.child("EmailID").getValue(String.class);
+                String mobile = dataSnapshot.child("MobileNo").getValue(String.class);
+                //Toast.makeText(UserProfile.this, email+" "+mobile, Toast.LENGTH_SHORT).show();
+                if(TextUtils.isEmpty(email)||TextUtils.isEmpty(mobile)){
+                    Toast.makeText(UserProfile.this, "Email and Mobile not found", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    textViewUserProfileMailValue.setText(email);
+                    textViewUserProfilePhoneValue.setText(mobile);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
         //On Pressing Back button
         buttonBackUserProfile.setOnClickListener(new View.OnClickListener() {
